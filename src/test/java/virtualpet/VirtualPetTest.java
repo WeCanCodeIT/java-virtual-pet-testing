beb7e6471abf7058c342ed6dc157b3c164933917
B T Forsythe <brian@btforsythe.io>
2018-01-26 15:18:51 -0500

    removed html

--- src/test/java/virtualpet/VirtualPetTest.java
+++ src/test/java/virtualpet/VirtualPetTest.java
@@ -6,34 +6,28 @@ import org.junit.Test;
 public class VirtualPetTest {
 
 	/**
-	 * <p>
 	 * In addition explicitly asserted conditions, some things are implicitly
 	 * tested. The only explicit assertion this test makes is that the hunger value
 	 * is changed, but it implicitly verifies that:
-	 * </p>
+	 *
+	 * - an initial hunger can be passed to the constructor
 	 * 
-	 * <ul>
-	 * <li>an initial hunger can be passed to the constructor</li>
-	 * <li>a <code>feed()</code> method exists</li>
-	 * <li>a <code>getHunger()</code> (accessor) method is implemented to return the
-	 * hunger value</li>
-	 * </ul>
+	 * - a feed() method exists
 	 * 
-	 * <p>
+	 * - a getHunger() (accessor) method is implemented to return the hunger value
+	 * 
 	 * I have decided that feeding is going to reduce hunger by 5, so to make this
-	 * test pass, I don't need to do anything more than have <code>getHunger()</code>
-	 * return 15.
-	 * </p>
+	 * test pass, I don't need to do anything more than have getHunger() return 15.
 	 */
 	@Test
 	public void shouldBeLessHungryAfterFeeding() {
 		// arrange
 		int initialHunger = 20;
 		VirtualPet underTest = new VirtualPet(initialHunger);
-		
+
 		// act
 		underTest.feed();
-		
+
 		// assert
 		int currentHunger = underTest.getHunger();
 		Assert.assertEquals(15, currentHunger);
